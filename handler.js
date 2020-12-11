3e809ea9e3fd497e1289461005a8a0e3a617eba6
Jared <jarednutt@outlook.com>
2020-12-10 20:01:31 -0800

    adding cron

--- handler.js
+++ handler.js
@@ -79,7 +79,7 @@ module.exports.transferFunds = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: "TODO: transferFunds",
         input: event,
       },
       null,
